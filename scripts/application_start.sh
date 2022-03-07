33cebdea404140524c580a0498e817e8757bd377
skssandy <89083444+skssandy@users.noreply.github.com>
2022-03-07 10:39:59 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-
-# Stop all servers and start the server as a daemon
-#cd /home/ubuntu/reactjs
-#node app.js
-  echo Nothing to do in the application step...
+echo "Nothing to do in the application step."
